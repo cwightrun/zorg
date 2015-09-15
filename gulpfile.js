e89998d221505fbc07de323c3824eb7bb17e8dcb
Casey Wight <casey@chapterthree.com>
2015-09-14 17:43:50 -0700

    Adding SVG-based utils and funcs

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
-// Include Gulp
+// Include Gulp, duh
 var gulp = require('gulp'); 
- 
+
 var imagemin      = require('gulp-imagemin'),
     minifyCSS     = require('gulp-minify-css'),
+    newer         = require('gulp-newer'),
     notify        = require('gulp-notify'),
     plumber       = require('gulp-plumber'),
     prefix        = require('gulp-autoprefixer'),
     prefix        = require('gulp-autoprefixer'),
+    rename        = require('gulp-rename'),
     sass          = require('gulp-sass'),
     sourcemaps    = require('gulp-sourcemaps'),
+    svgmin        = require('gulp-svgmin'),
+    svgstore      = require('gulp-svgstore'),
     watch         = require('gulp-watch'),
     util          = require('gulp-util');
 
@@ -41,9 +45,23 @@ gulp.task('imagemin', function() {
   var imgSrc = 'images/src/**/*',
       imgDst = 'images/';
   gulp.src(imgSrc)
+    .pipe(newer(imgDst))
     .pipe(imagemin())
     .pipe(gulp.dest(imgDst));
-})
+});
+
+// SVG Icon nonsense
+gulp.task('icons', function () {
+  var iconSrc = 'images/icons/*.svg',
+      iconDest = 'images/';
+
+  return gulp.src(iconSrc)
+    .pipe(svgmin())
+    .pipe(rename({prefix: 'icon-'}))
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(newer(iconDest))
+    .pipe(gulp.dest(iconDest));
+});
 
 // Gulp Sass Task 
 gulp.task('sass', function() {
@@ -56,21 +74,10 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./css'));
 });
 
-// // Gulp Sass Task 
-// gulp.task('sass', function() {
-//   gulp.src('sass/**/*.scss')
-//     .pipe(sourcemaps.init()) // Initializes sourcemaps
-//     .pipe(prefix('last 2 versions', '> 1%', 'ie 8'))
-//     .pipe(sourcemaps.write()) // Writes sourcemaps into the CSS file
-//     .pipe(minifyCSS({
-//       // keepBreaks: true,
-//       // keepSpecialComments: 0
-//     }))
-//     .pipe(gulp.dest('css'));
-// })
-
 gulp.task('watch', function() {
-  gulp.watch('sass/**/*.scss', ['sass'])
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('images/icons/*.svg', ['icons']);
+  gulp.watch('images/src/**/*', ['imagemin']);
 })
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'imagemin', 'icons', 'watch']);
